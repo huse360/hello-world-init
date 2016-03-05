232672e5b2ffc8b664949d7b8a9e57eb9ab8e415
huse <huse@huseMBP.local>
2016-03-06 01:40:43 +0900

    Initial commit

---
+++ HelloWorld/HelloWorld/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HelloWorld
+//
+//  Created by huse on 3/6/16.
+//  Copyright (c) 2016 satoruna. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
